1cfa6d318be6395f8849f3fe5e309214558bc0f3
archer <adilking0071@gmail.com>
2023-10-03 06:44:54 +0500

    added uvicorn

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.api.product import endpoints as product_endpoints
-from app.api.inventory import endpoints as inventory_enpoints
-from api.sales import endpoints as sales_endpoints
+from api.product import endpoints as product_endpoints
+from api.inventory import endpoints as inventory_enpoints
+import uvicorn
 
 app = FastAPI()
 
@@ -16,4 +16,7 @@ app.mount("/uploads", StaticFiles(directory=static_dir), name="uploads")
 # Routes
 app.include_router(product_endpoints.router, prefix="/products", tags=["products"])
 app.include_router(inventory_enpoints.router,prefix="/inventory",tags=["inventory"])
-app.include_router(sales_endpoints.router,prefix="/inventory",tags=["inventory"])
+# app.include_router(sales_endpoints.router,prefix="/inventory",tags=["inventory"])
+
+if __name__ == '__main__':
+    uvicorn.run(app)
